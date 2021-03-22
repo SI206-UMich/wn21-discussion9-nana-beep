e465fe175f4b8824afd5f8c0ab5475ecb9b1e3c0
jinanaki <jinanaki@umich.edu>
2021-03-22 16:57:35 -0400

    implemented all three functions

--- diss_9.py
+++ diss_9.py
@@ -6,33 +6,54 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+  #  soupy = soup.find('a', {'href' : '/pokedex/charizard'})
+  #  print(soupy)
+  #  soupyy = soupy.find('a', {'href' : '/pokedex/charmander'})
+   # return 'https://pokemondb.net' + soupy.get('href') #+ soupyy.get('href')
+    anchor = soup.find('div',class_='infocard-list infocard-list-pkmn-lg')
+    anchor2 = anchor.find_all('span',class_='infocard-lg-img')[3]
+    anchor3 = anchor2.find('a')['href']
+    return 'https://pokemondb.net'+anchor3
+
+
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
+    new_list = []
+
     url = 'https://pokemondb.net/pokedex/'+pokemon
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    table = soup.find_all('table', class_ = 'data-table')[2]
+    body = table.find('tbody')
+
+    tags = body.find_all('tr')
+
+    for tag in tags:
+        t = tag.find('td', class_ = 'cell-name')
+        new_t = t.find('a', class_ = 'ent-name').text
+        new_list.append(new_t)
+
+    return new_list
     #add code here
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    new_list = []
 
     # define the regular expression
+    for line in sentences:
+        x = re.findall(r'@(\d{1,2}\s?(?:am|pm))', line)
+
+        for i in x:
+            new_list.append(i)
     
+    return new_list
 
-    # loop through each sentence or phrase in sentences
-    
-
-    # find all the words that match the regular expression in each sentence
-       
-
-    # loop through the found words and add the words to your empty list
-
-
-    #return the list of the last letter of all words that begin or end with a capital letter
 
 
 
